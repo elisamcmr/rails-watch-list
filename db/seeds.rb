5c8bd66996064e7ba381a93c050d180c37e6987c
Elisabeth Camara <ecamara.k@gmail.com>
2024-11-14 12:23:04 +0100

    models and seeding

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,28 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+require 'open-uri'
+require 'json'
+
+# 1. Clean the database 🗑️
+puts 'Cleaning database...'
+Movie.destroy_all
+
+# 2. Create the instances 🏗️
+puts 'Creating movies...'
+
+url = "https://tmdb.lewagon.com/movie/top_rated"
+
+movies_serialized = URI.open(url).read
+movies = JSON.parse(movies_serialized)
+
+movies['results'].each do |movie_data|
+  Movie.create!(
+    title: movie_data['title'],
+    overview: movie_data['overview'],
+    poster_url: "https://image.tmdb.org/t/p/w500#{movie_data['poster_path']}",
+    rating: movie_data['vote_average']
+  )
+end
+
+puts "#{Movie.count} movies created"
